9ef82b450bab9c5347762446109f2d422d8c6ebf
johncorby <williamforgames6@gmail.com>
2020-04-12 00:02:22 -0700

    this is how we are going to do things

--- src/main/kotlin/com/johncorby/gravityguild/Main.kt
+++ src/main/kotlin/com/johncorby/gravityguild/Main.kt
@@ -7,6 +7,16 @@ import org.bukkit.plugin.java.JavaPlugin
 
 lateinit var PLUGIN: Main
 
+/*
+todo
+    [this models hypixel and is the alternative to choosing to world/instance somehow yourself]
+    instead of arenas being stored with bases and instances,
+    we'll simply have arena worlds be saved (and maybe keep track of them via a list of worlds).
+    then, when a player initiates a joins (probably by clicking a sign),
+    we simply create an arena object that clones one of the arena worlds to use as an instance.
+    OR
+    if there already exists running games, we place the player into the one with the most players already in it.
+ */
 class Main : JavaPlugin() {
     override fun onEnable() {
         PLUGIN = this
