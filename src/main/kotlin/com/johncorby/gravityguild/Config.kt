e2a73563456e632e1f60ac8e085e2fb6db394c62
JohnCorby <williamforgames6@gmail.com>
2020-05-16 19:16:05 -0700

    type safety with config sorta (throws own error)

--- src/main/kotlin/com/johncorby/gravityguild/Config.kt
+++ src/main/kotlin/com/johncorby/gravityguild/Config.kt
@@ -4,17 +4,17 @@ import com.johncorby.coreapi.ConfigFile
 import org.bukkit.Location
 
 object Config : ConfigFile() {
-    val MIN_PLAYERS by Field("players.min", 2)
-    val MAX_PLAYERS by Field("players.max", 10)
-    val LIVES by Field("players.lives", 3)
+    val MIN_PLAYERS by field("players.min", 2)
+    val MAX_PLAYERS by field("players.max", 10)
+    val LIVES by field("players.lives", 3)
 
-    val COUNTDOWN_INTERVALS by Field("countdown-intervals", listOf(20, 10, 5, 4, 3, 2, 1))
-    val COOLDOWN_TIME by Field("cooldown-time", 5)
-    val WIN_WAIT_TIME by Field("win-wait-time", 5)
+    val COUNTDOWN_INTERVALS by field("countdown-intervals", listOf(20, 10, 5, 4, 3, 2, 1))
+    val COOLDOWN_TIME by field("cooldown-time", 5)
+    val WIN_WAIT_TIME by field("win-wait-time", 5)
 }
 
 
 object Data : ConfigFile("data.yml") {
-    var lobby by Field<Location?>("lobby")
+    var lobby by field<Location?>("lobby")
 }
 
